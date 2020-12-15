2d9000c3837fddbf5046df815d4ca2a0cba9ba5f
byulyi kim <byulyikeem@gmail.com>
2020-12-15 13:53:05 +0900

    Add: Footer Component 작업 완료

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/styles/reset.scss';
 import '../src/styles/common.scss';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 import Routes from './Routes';
 
