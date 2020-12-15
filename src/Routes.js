9b89012dd595886b2af5c2a83beb18ac0e2228ae
jjuny0113 <jjuny920113@gmail.com>
2020-12-14 17:56:34 -0800

    reset

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MainPage from './Pages/MainPage/MainPage';
+import Login from './Pages/Login/Login';
+import Detail from './Pages/Detail/Detail';
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/main" component={Main} />
+          <Route exact path="/mainpage" component={MainPage} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/details" component={Details} />
-          
+          <Route exact path="/details" component={Detail} />
         </Switch>
       </Router>
     );
