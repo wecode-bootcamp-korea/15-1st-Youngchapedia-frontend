d1ebc0a36ce8218a1078ddee233a358578495ed8
jjuny0113 <jjuny920113@gmail.com>
2020-12-23 17:49:25 -0800

    Modify: Route 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainPage from './Pages/MainPage/MainPage';
 import Detail from './Pages/Detail/Detail';
 import MyPageMain from './Pages/MyPage/MyPageMain';
 import MyPageFooterDetail from './Pages/MyPage/Components/MyPageFooterDetail/MyPageFooterDetail';
 import Error from './Pages/Error/Error';
+import DetailMainCommentLink from './Pages/Detail/DetailMain/Components/DetailMainComment/DetailMainCommentModal/DetailMainCommentModal';
 import '@fortawesome/fontawesome-free/js/all.js';
 import FilterPage from './Pages/FilterPage/FilterPage';
 import SearchResultPage from './Components/Nav/Search/SearchResultPage';
@@ -18,6 +19,7 @@ class Routes extends React.Component {
           <Route exact path="/details" component={Detail} />
           <Route exact path="/mypage" component={MyPageMain} />
           <Route exact path="/error" component={Error} />
+          <Route exact path="/login" component={Login} />
           <Route exact path="/details" component={Detail} />
           <Route exact path="/mypage" component={MyPageMain} />
           <Route
@@ -25,7 +27,8 @@ class Routes extends React.Component {
             path="/mypagefooterdetail"
             component={MyPageFooterDetail}
           />
-          <Route exact path="/details" component={Detail} />
+
+          <Route exact path="/detail" component={Detail} />
           <Route exact path="/filter/:id" component={FilterPage} />
           <Route exact path="/movies/:id" component={Detail} />
           <Route exact path="/details/:id" component={Detail} />
